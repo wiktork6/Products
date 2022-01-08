bf615705a3016bd67fec462281975ca1082bc0d4
wiktork6 <wiktork6@gmail.com>
2022-01-08 08:33:38 +0100

    Fixed error with updatating a product

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -2,7 +2,7 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
 import {IsString, Length} from "class-validator";
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {
+export class UpdateProductDto extends CreateProductDto {
     @IsString()
     @Length(24,24)
     id: string
